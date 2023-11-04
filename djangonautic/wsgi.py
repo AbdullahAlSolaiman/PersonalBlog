093ea5621c5b33a46de4db8546a82711c916c83e
Abdullah Al Sulaiman <abdulla111222@gmail.com>
2023-11-04 21:42:56 +0300

    fixed wsgi

--- djangonautic/wsgi.py
+++ djangonautic/wsgi.py
@@ -13,6 +13,9 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangonautic.settings")
 
+if os.environ.get('ENV') == 'production':
+    sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 application = get_wsgi_application()
 
 app = application
